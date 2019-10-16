8c02d3e10b05c11e4445cc423f298cb5e80cac16
Chiter <chinaric@tut.by>
2019-10-16 22:20:37 +0200

    Initial commit

---
+++ src/Liner/Task1/Function.java
@@ -0,0 +1,22 @@
+package Liner.Task1;
+
+/*
+
+Найдите значение функции: z= ((a - 3) * b / 2) + c
+
+ */
+
+public class Function {
+    public static void main (String[] args) {
+        double z;
+        double a = 1.1;
+        double b = 2.76;
+        double c = 33;
+
+
+        z = ((a - 3) * b / 2) + c;
+
+        System.out.println (z);
+
+    }
+}
