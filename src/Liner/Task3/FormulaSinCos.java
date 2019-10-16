fd64a099e9d4ee3e8d3794fdb7d4930fd73d49ac
Chiter <chinaric@tut.by>
2019-10-16 22:45:20 +0200

    task solution

---
+++ src/Liner/Task3/FormulaSinCos.java
@@ -0,0 +1,21 @@
+package Liner.Task3;
+
+/*
+
+Вычислить значение выражения по формуле (все переменные принимают действительные значения:
+((Sin x + Cos y) / (Cos x - Sin y)) * Tg xy
+
+ */
+
+public class FormulaSinCos {
+    public static void main(String[] args) {
+        double x = 1;
+        double y = 1;
+        double value;
+
+        value = ((Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y))) * Math.tan(x * y);
+        System.out.println(value);
+
+
+    }
+}
